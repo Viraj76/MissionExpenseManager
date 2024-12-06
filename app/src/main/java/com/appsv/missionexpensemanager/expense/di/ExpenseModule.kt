71f47fdc3518d26b5fe012a8f1364470ef35ffd0
Viraj76 <virajg.7680@gmail.com>
2024-12-06 11:03:26 +0530

    ExpenseModule - providing firebase database instance

---
+++ app/src/main/java/com/appsv/missionexpensemanager/expense/di/ExpenseModule.kt
@@ -0,0 +1,22 @@
+package com.appsv.missionexpensemanager.expense.di
+
+import com.google.firebase.database.FirebaseDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+object ExpenseModule {
+
+    @Provides
+    @Singleton
+    fun provideFirebaseDatabaseInstance() : FirebaseDatabase{
+        return FirebaseDatabase.getInstance()
+    }
+
+
+}
